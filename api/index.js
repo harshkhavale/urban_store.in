6fdb7e06bfcb96dd75b39d34514866d341094c68
Harsh Khavale <harshkhavale1102@gmail.com>
2023-12-31 19:47:37 +0530

    little try!

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 const app = express();
 
 const corsOptions ={
-  origin:'https://urban-store-in-apk.vercel.app/', 
+  origin:'https://urban-store-in-apk.vercel.app', 
   credentials:true,             //access-control-allow-credentials:true
   optionSuccessStatus:200
 }
